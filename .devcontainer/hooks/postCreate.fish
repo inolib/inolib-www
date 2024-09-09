e7e977fc85f1965a5cf65ef7e0ca58fff2c097e2
Matthieu Meignan <matthieu.meignan@inolib.com>
2024-09-09 21:43:23 +0000

    devcontainer

---
+++ .devcontainer/hooks/postCreate.fish
@@ -0,0 +1,10 @@
+# fisher
+curl -sL 'https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish' | source
+
+fisher install 'jorgebucaran/fisher'
+fisher install 'jorgebucaran/autopair.fish'
+fisher install 'nickeb96/puffer-fish'
+fisher install 'meaningful-ooo/sponge'
+
+# corepack
+corepack up
