473c5afe6864d67494a4d41e4677b09d922bd182
Matthieu Meignan <matthieu.meignan@inolib.com>
2024-09-09 22:51:30 +0000

    config

--- .eslintrc.js
+++
@@ -1,8 +0,0 @@
-/** @type {import("eslint").Linter.Config} */
-export default {
-  extends: ["next/core-web-vitals"],
-  rules: {
-    "import/no-anonymous-default-export": "off",
-    "react/no-unescaped-entities": "off",
-  },
-};
