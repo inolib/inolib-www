5437abe29900d1a0ea8ef8b7a8b024350d94ad0a
mariabens <maria.grairi33@hotmail.com>
2024-09-09 16:21:56 +0200

    build3

--- src/app/(pages)/search-results/page.tsx
+++ src/app/(pages)/search-results/page.tsx
@@ -2,7 +2,7 @@ import Header from '~/components/Header';
 import HeaderHat from '~/components/HeaderHat';
 
 import Footer from '~/components/Footer';
-import {Suspense} from 'React'
+
 import dynamic from 'next/dynamic'
 
 const SearchResultsPage = dynamic(() => import('~/app/(pages)/search-results/main'), { ssr: false }); 
