e9a5d2e57dbe665b3309a25e71ec5ec9d7caf0e9
Matthieu Meignan <matthieu.meignan@inolib.com>
2024-09-10 09:08:51 +0000

    add tsup and tsx

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import { createServer } from "node:http";
+import { parse } from "node:url";
+import next from "next";
+
+(async () => {
+  const dev = process.env.NODE_ENV !== "production";
+  const hostname = "localhost";
+  const port = parseInt(process.env.PORT ?? "3000", 10);
+
+  const app = next({ dev, hostname, port });
+
+  await app.prepare();
+
+  createServer((req, res) => {
+    app.getRequestHandler()(req, res, parse(req.url as string, true));
+  }).listen(port);
+
+  console.log(`Server listening on http://${hostname}:${port} (${dev ? "development" : process.env.NODE_ENV})`);
+})();
