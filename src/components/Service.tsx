d0cb2cce9cde6d3208a33b4b140884ec1c7759ad
mariabens <maria.grairi33@hotmail.com>
2024-09-10 22:11:51 +0200

    responsive

--- src/components/Service.tsx
+++ src/components/Service.tsx
@@ -1,10 +1,10 @@
-import { ArrowRightIcon } from "@heroicons/react/20/solid";
 import Image from "next/image";
+import { ArrowRightIcon } from "@heroicons/react/20/solid";
 
-const Services = () => {
+export default function Services() {
   return (
     <section className="bg-[#FCFBF9] py-20" aria-labelledby="services-heading">
-      <div className="container mx-auto px-4">
+      <div className="container px-4">
         <div className="mb-12 text-center">
           <span
             className="text-black-500 inline-block rounded-full bg-[#CBE0E4] px-3 py-1 text-sm"
@@ -12,7 +12,7 @@ const Services = () => {
           >
             Services
           </span>
-          <h2 id="services-heading" className="mb-4 mt-4 text-3xl font-bold md:text-4xl">
+          <h2 id="services-heading" className="mb-4 mt-4 text-3xl lg:font-semibold xxs:font-semibold md:text-4xl xxs:w-[320px] lg:w-auto xxs:mx-auto xxs:text-3xl md:w-256">
             Découvrez nos services proposés autour d'accessibilité numérique
           </h2>
           <p className="mx-auto max-w-2xl text-gray-600">
@@ -21,19 +21,19 @@ const Services = () => {
           </p>
         </div>
 
-        <div className="space-y-12 xxs:flex-col">
+        <div className="space-y-12 xxs:flex-col xxs:ml-1">
           <div className="flex flex-col items-center py-6 xxs:py-2 xs:py-3 md:flex-row">
             <div>
-              <div className="mx-10 max-w-128">
+              <div className="lg:mx-10 max-w-128 xxs:mr-12  flex-col">
                 <Image src="/Icons/fichier.svg" alt="" width={40} height={40}></Image>
 
-                <h3 className="mb-2 mt-2 pt-2 text-2xl font-semibold">Audits d'accessibilité numérique</h3>
+                <h3 className="mb-2 mt-2 pt-2 text-2xl font-semibold xxs:w-92 ">Audits d'accessibilité numérique</h3>
                 <p className="mb-4 text-gray-600">
                   Bénéficiez d'audits d'accessibilité réalisés par nos experts, mettant en évidence les points forts et
                   les zones d'amélioration de votre site ou application. Nous proposons des audits de qualité pour vous
                   assurer de respecter les normes d'accessibilité.
                 </p>
-                <ul className="mb-4 list-disc pl-5">
+                <ul className="mb-4 list-disc pl-5 xxs:pl-1">
                   <li className="Items-start flex py-1">
                     <Image src="/Icons/CheckIcon.svg" alt="" width={20} height={20} className="mr-2" />
                     Corriger les erreurs en incluant tous les utilisateurs
@@ -57,13 +57,13 @@ const Services = () => {
               </div>
             </div>
 
-            <div className="mb-6 ml-24 flex-shrink-0 md:mb-0 md:mr-6">
-              <Image src="/Homepage/image1.svg" alt="" width={550} height={550} />
+            <div className="mb-6 ml-24 flex-shrink-0 md:mb-0 md:mr-6 xxs:mt-8 ">
+              <Image src="/Homepage/image1.svg" alt="" width={550} height={550} className=""/>
             </div>
           </div>
 
           <div className="flex flex-col items-center justify-center md:flex-row">
-            <div className="mb-6 flex-shrink-0 md:mb-0 md:mr-6 lg:mr-60">
+            <div className="mb-6 flex-shrink-0 md:mb-0 md:mr-6 lg:mr-60 order-1 md:order-none">
               <Image src="/Homepage/sommet.svg" alt="" width={400} height={400} />
             </div>
             <div className="mr-8 max-w-128 flex-col">
@@ -73,7 +73,7 @@ const Services = () => {
                 Nos experts vous assistent techniquement et vous accompagnent dans vos projets, avec des services
                 sur-mesure.
               </p>
-              <ul className="mb-4 list-disc pl-5">
+              <ul className="mb-4 list-disc pl-5 xxs:pl-1">
                 <li className="Items-start flex py-1">
                   <Image src="/Icons/CheckIcon.svg" alt="" width={20} height={20} className="mr-2" />
                   Rendez-vous et séances d'accompagnement via le chat
@@ -95,17 +95,18 @@ const Services = () => {
                 <ArrowRightIcon className="ml-2 h-5 w-5" />
               </a>
             </div>
+
           </div>
 
           <div className="flex flex-col items-center py-6 md:flex-row">
-            <div className="max-w-128 flex-col px-10">
+            <div className="lg:w-128 flex-col px-10 xxs:mr-14 xxs:w-80">
               <Image src="/Icons/mobile.svg" alt="" width={40} height={40}></Image>
               <h3 className="mb-2 mt-2 pt-2 text-2xl font-semibold">Développement web et mobile</h3>
               <p className="mb-4 text-gray-600">
                 Optez pour des sites web et applications conformes aux normes d'accessibilité grâce à notre équipe de
                 développeurs qualifiés.
               </p>
-              <ul className="mb-4 list-disc pl-5">
+              <ul className="mb-4 list-disc pl-5 xxs:pl-1">
                 <li className="Items-start flex py-1">
                   <Image src="/Icons/CheckIcon.svg" alt="" width={20} height={20} className="mr-2" />
                   Projets sur-mesure, de la conception à la mise en ligne
@@ -127,13 +128,13 @@ const Services = () => {
                 <ArrowRightIcon className="ml-2 h-5 w-5" />
               </a>
             </div>
-            <div className="mb-6 flex-shrink-0 px-36 md:mb-0 md:mr-6">
+            <div className="mb-6 xxs:ml-32 lg:ml-20 flex-shrink-0 md:mb-0 md:mr-6 xxs:mt-8 order-1 md:oeder-none ">
               <Image src="/Homepage/macon.svg" alt="" width={400} height={400} />
             </div>
           </div>
 
           <div className="flex flex-col items-center md:flex-row">
-            <div className="mb-6 flex-shrink-0 md:mb-0 md:mr-6 lg:mr-60">
+            <div className="mb-6 flex-shrink-0 md:mb-0 md:mr-6 lg:mr-60 order-1 md:order-none xxs:mt-8">
               <Image src="/Homepage/seated 2.svg" alt="" width={400} height={400} />
             </div>
             <div className="mr-12 max-w-128 flex-col">
@@ -143,7 +144,7 @@ const Services = () => {
                 Participez à des formations spécialisées sur les normes et techniques d'accessibilité numérique, animées
                 par nos experts.
               </p>
-              <ul className="mb-4 list-disc pl-5">
+              <ul className="mb-4 list-disc pl-1">
                 <li className="Items-start flex py-1">
                   <Image src="/Icons/CheckIcon.svg" alt="" width={20} height={20} className="mr-2" />
                   Sessions de formation en direct et à distance
@@ -170,6 +171,4 @@ const Services = () => {
       </div>
     </section>
   );
-};
-
-export default Services;
+}
