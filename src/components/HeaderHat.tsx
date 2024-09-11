2d218db2c9c7af5c861908bec73dbcd0e1d5df93
ZarakiMelo <chahbisamy@hotmail.fr>
2024-09-11 14:41:52 +0200

    HeaderHat component responsive

--- src/components/HeaderHat.tsx
+++ src/components/HeaderHat.tsx
@@ -28,48 +28,39 @@ const HeaderHat = ({ bgColor, textColor, buttonVariant, link }: HeaderHatProps) 
   };
 
   return (
-    <header className={`relative ${bgColor} ${textColor} flex h-20 justify-center 2xl:pl-6`}>
-      <div className="flex h-full w-full items-center justify-between 2xl:w-[1536px]">
-        {/* Lien d'accès au contenu principal */}
-        {!isSearchOpen && (
-          <Link
-            href={link}
-            className={`${textColor} ml-8 flex flex-row font-semibold hover:text-gray-400`}
-            aria-label="Accéder au contenu principal"
-          >
-            Accéder au contenu
-            <svg
-              className="ml-2 mt-[5px] h-4 w-4"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 19l7-7-7-7"></path>
-            </svg>
-          </Link>
-        )}
-
-        {/* Barre de recherche */}
-        <div>
-          <SearchBar color={textColor} isSearchOpen={isSearchOpen} setIsSearchOpen={setIsSearchOpen} />
-        </div>
-
-        {/* Bouton "Nous contacter" */}
-        <Button
-          variant={buttonVariant}
-          aria-label="Nous contacter"
-          onClick={handleLink}
-          className="mr-8 hidden lg:block"
+    <header
+      className={`relative ${bgColor} ${textColor} flex items-center justify-between p-6 lg:justify-around lg:space-x-56`}
+    >
+      {!isSearchOpen && (
+        <Link
+          href={link}
+          className={`${textColor} flex flex-row font-semibold hover:text-gray-400`}
+          aria-label="Accéder au contenu"
         >
-          Nous contacter
-        </Button>
-
-        {/* Élément pour informer les lecteurs d'écran après la redirection */}
-        <div aria-live="polite" className="sr-only">
-
-        </div>
+          Accéder au contenu
+          <svg
+            className="ml-2 mt-[5px] h-4 w-4"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 19l7-7-7-7"></path>
+          </svg>
+        </Link>
+      )}
+      <div className=" ">
+        <SearchBar isSearchOpen={isSearchOpen} setIsSearchOpen={setIsSearchOpen} />
       </div>
+      <Button
+        variant={buttonVariant}
+        aria-label="Nous contacter"
+        onClick={handleLink}
+        className="mr-20 hidden lg:block"
+      >
+        {" "}
+        Nous contacter{" "}
+      </Button>
     </header>
   );
 };
