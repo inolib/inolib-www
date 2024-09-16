adb944e7dadbe741783a6b6bf9a0a3a1325f7443
ZarakiMelo <chahbisamy@hotmail.fr>
2024-09-16 10:58:27 +0200

    e

--- src/components/Testimonial.tsx
+++ src/components/Testimonial.tsx
@@ -5,10 +5,7 @@ import { useState } from "react";
 
 import { testimonials } from "~/DATA/data";
 
-type TestimonialProps = {
-  aligned: boolean;
-};
-const Testimonials = (aligned: TestimonialProps) => {
+const Testimonials = () => {
   const [current, setCurrent] = useState(0);
 
   const nextTestimonial = () => {
@@ -18,7 +15,7 @@ const Testimonials = (aligned: TestimonialProps) => {
   const prevTestimonial = () => {
     setCurrent((prev) => (prev - 1 + testimonials.length) % testimonials.length);
   };
-  console.log("aligned", aligned.aligned);
+
   return (
     <div className="relative rounded-lg bg-[#111F22] p-8 text-white md:p-16">
       <div className="md:ml-20">
